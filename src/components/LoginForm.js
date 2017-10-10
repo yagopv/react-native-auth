2b7ab21633ec4c8c3afc534f2696d663a1d01041
Yago Pérez Vázquez <yago.perezv@tecnocom.es>
2017-10-10 13:49:09 +0200

    Finishing styling

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react'
 import { View } from 'react-native';
+import firebase from 'firebase';
+
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '', password: '' };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password);
+  }
 
   render() {
     return (
       <Card>
         <CardSection>
-          <Input 
-            value={this.state.text} 
-            onChangeText={(text) => this.setState({ text })} /> 
-        </CardSection> 
-
-        <CardSection>
-
+          <Input
+            label="Email"
+            placeholder="user@mail.com" 
+            value={this.state.email}
+            onChangeText={(email) => this.setState({ email })} />
         </CardSection>
 
         <CardSection>
-          <Button> 
+          <Input
+            label="Password"
+            placeholder="Enter your password here"
+            secureTextEntry
+            value={this.state.password}
+            onChangeText={(password) => this.setState({ password })} />
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Login
           </Button>
         </CardSection>
